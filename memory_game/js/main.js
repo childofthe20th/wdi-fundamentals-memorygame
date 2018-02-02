4a65078a8021b9622a59236469b2c0208383e594
Eric Sanchez <mojo@Erics-MacBook-Pro.local>
2018-02-02 11:00:03 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+console.log("Up and running!");
+
+var cards = ["queen", "king", "queen", "king"];
+// Created array for all four suits
+
+var cardsInPlay = [];
+// Created array for cards in play
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+// Defined first card and added it to cards in play array
 
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+// Defined second card and added it to cards in play array
+
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else  {
+		alert("Sorry, try again.");
+	}
+}
+// Set logic for matching two cards in play
